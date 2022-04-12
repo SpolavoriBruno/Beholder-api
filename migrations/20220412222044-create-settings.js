8cf06a91b2a21938c59ceb1f33cb417521df849c
Bruno J. Spolavori <bspol.323@gmail.com>
2022-04-12 20:56:14 -0300

    Fix indentation

--- migrations/20220412222044-create-settings.js
+++ migrations/20220412222044-create-settings.js
@@ -1,31 +1,31 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-   return queryInterface.createTable('settings', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
+    async up(queryInterface, Sequelize) {
+        return queryInterface.createTable('settings', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                allowNull: false,
+                primaryKey: true
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            apiUrl: Sequelize.STRING,
+            accessKey: Sequelize.STRING,
+            secretKey: Sequelize.STRING,
+            createdAt: Sequelize.DATE,
+            updatedAt: Sequelize.DATE
+        })
     },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    apiUrl: Sequelize.STRING,
-    accessKey: Sequelize.STRING,
-    secretKey: Sequelize.STRING,
-    createdAt: Sequelize.DATE,
-    updatedAt: Sequelize.DATE
-})
-  },
 
-  async down (queryInterface, Sequelize) {
-   return queryInterface.dropTable('settings')
-  }
+    async down(queryInterface, Sequelize) {
+        return queryInterface.dropTable('settings')
+    }
 };
