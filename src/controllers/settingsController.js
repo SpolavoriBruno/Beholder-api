0cbd5ebc3c041d8844cf05b7124340612980dc22
Bruno J. Spolavori <bspol.323@gmail.com>
2022-04-13 15:19:31 -0300

    Remove moked settings

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -1,3 +1,9 @@
+const { getSettings } = require('../repositories/settingsRepository')
+
 exports.getSettings = (req, res, next) => {
-    res.json({ email: "teste@teste.com" })
+    const id = res.locals.token.id
+    console.log(id)
+    getSettings(id)
+        .then(settings => res.json(settings))
+        .catch(error => res.json(error).status(500))
 }
