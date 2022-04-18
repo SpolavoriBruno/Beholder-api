c949f69e8253ba1993e6bbed12f58166a429cda9
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-18 00:17:38 -0300

    Update sequelize config

--- src/db.js
+++ src/db.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
+    process.env.DB_NAME || 'beholder',
+    process.env.DB_USER || 'root',
     process.env.DB_PASSWORD,
     {
-        dialect: process.env.DB_DIALECT,
-        host: process.env.DB_HOST
+        dialect: process.env.DB_DIALECT || 'postgres',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || '5432',
+        logging: process.env.DB_LOGS === 'true',
     }
 )
 
