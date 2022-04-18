ffe804bfbab850350161b4c97ca8fea03e554721
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-18 01:22:37 -0300

    Secure Web Socket Server

--- src/app-ws.js
+++ src/app-ws.js
@@ -1,4 +1,6 @@
 const Websockets = require('ws')
+const jwt = require('jsonwebtoken')
+const { isBlacklisted } = require('./controllers/authController')
 
 function onMessage(data) {
     console.log(data);
@@ -14,8 +16,30 @@ function onConnection(ws, req) {
     console.info('New Client Connected')
 }
 
+function corsValidation(origin) {
+    return process.env.CORS_ORIGIN.startsWith(origin)
+}
+
+function verifyClient(info, callback) {
+    if (!corsValidation(info.origin)) return callback(false, 401)
+    const token = info.req.url.split('token=')[1]
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            if (decoded && !isBlacklisted(token))
+                return callback(true)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return callback(false, 401)
+}
+
 module.exports = server => {
-    const wss = new Websockets.Server({ server })
+    const wss = new Websockets.Server({
+        server,
+        verifyClient: verifyClient
+    })
     console.info('WebSocket Server is running')
 
     wss.on('connection', onConnection)
