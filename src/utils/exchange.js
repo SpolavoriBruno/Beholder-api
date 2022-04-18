845e559c990a13ace0fa038fc2a979ddcced2858
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-17 23:38:00 -0300

    Dont repeat your self

--- src/utils/exchange.js
+++ src/utils/exchange.js
@@ -2,12 +2,19 @@ const Binance = require('node-binance-api')
 
 module.exports = settings => {
     if (!settings) throw new Error('Settings are required to connect to exchange')
+    if (!settings.apiUrl) throw new Error('API URL is required to connect to exchange')
+    if (!settings.streamUrl) throw new Error('Stream URL is required to connect to exchange')
+
+    function formatUrl(url) {
+        return url.endsWith('/') ? url : `${url}/`
+    }
 
     const binance = new Binance({
         APIKEY: settings.accessKey,
         APISECRET: settings.secretKey,
         urls: {
-            base: settings.apiUrl.endsWith('/') ? settings.apiUrl : `${settings.apiUrl}/`,
+            base: formatUrl(settings.apiUrl),
+            stream: formatUrl(settings.streamUrl)
         }
     })
 
