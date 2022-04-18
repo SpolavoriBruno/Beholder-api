dc2b0b8b5e30751fe3e968640e977f092c41bddc
Bruno José Spolavori <bspol.323@gmail.com>
2022-04-18 01:23:14 -0300

    Fix route exchange/balance

--- src/routers/exchangeRouter.js
+++ src/routers/exchangeRouter.js
@@ -3,6 +3,6 @@ const { getBalance } = require('../controllers/exchangeController')
 
 const router = express.Router()
 
-router.get('/', getBalance)
+router.get('/balance', getBalance)
 
 module.exports = router
