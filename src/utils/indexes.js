5c5876d44328ddabdb05679edd6ae778994da86a
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-04 02:46:56 -0300

    Add indexes

--- src/utils/indexes.js
+++ src/utils/indexes.js
@@ -3,36 +3,118 @@ const technicalindicators = require('technicalindicators')
 exports.INDEX_KEYS = {
     RSI: 'RSI',
     MACD: 'MACD',
-    WALLET: 'WALLET',
-    LAST_ORDER: 'LAST_ORDER',
-    LAST_CANDLE: 'LAST_CANDLE',
+    SMA: 'SMA',
+    EMA: 'EMA',
+    STOCH_RSI: 'SRSI',
+    BOLLINGER_BAND: 'BB',
 }
 
 exports.RSI = (closes, period = 14) => {
-    const rsiResult = technicalindicators.rsi({
-        period,
+    const rsi = technicalindicators.rsi({
+        period: parseInt(period),
         values: closes
     })
 
     return {
-        current: rsiResult[rsiResult.length - 1],
-        previous: rsiResult[rsiResult.length - 2]
+        current: rsi[rsi.length - 1],
+        previous: rsi[rsi.length - 2]
     }
 }
 
 
 exports.MACD = (closes, fastPeriod = 12, slowPeriod = 26, signalPeriod = 9) => {
-    const macdResult = technicalindicators.macd({
+    const macd = technicalindicators.macd({
         SimpleMAOscillator: false,
         SimpleMASignal: false,
         values: closes,
-        signalPeriod,
-        fastPeriod,
-        slowPeriod,
+        signalPeriod: parseInt(signalPeriod),
+        fastPeriod: parseInt(fastPeriod),
+        slowPeriod: parseInt(slowPeriod),
     })
 
     return {
-        current: macdResult[macdResult.length - 1],
-        previous: macdResult[macdResult.length - 2]
+        current: macd[macd.length - 1],
+        previous: macd[macd.length - 2]
     }
+}
+
+exports.SMA = (closes, period = 10) => {
+    const sma = technicalindicators.sma({
+        period: parseInt(period),
+        values: closes
+    })
+    return {
+        current: sma[sma.length - 1],
+        previous: sma[sma.length - 2]
+    }
+}
+
+exports.EMA = (closes, period = 10) => {
+    const ema = technicalindicators.ema({
+        period: parseInt(period),
+        values: closes
+    })
+    return {
+        current: ema[ema.length - 1],
+        previous: ema[ema.length - 2]
+    }
+}
+
+exports.StochRSI = (closes, dPeriod = 3, kPeriod = 3, rsiPeriod = 14, stochasticPeriod = 14) => {
+    const stochRSI = technicalindicators.stochRSI({
+        dPeriod: parseInt(dPeriod),
+        kPeriod: parseInt(kPeriod),
+        rsiPeriod: parseInt(rsiPeriod),
+        stochasticPeriod: parseInt(stochasticPeriod),
+        values: closes
+    })
+    return {
+        current: stochRSI[stochRSI.length - 1],
+        previous: stochRSI[stochRSI.length - 2]
+    }
+}
+
+exports.BollingerBands = (closes, period = 20, stdDev = 2) => {
+    const bollingerBands = technicalindicators.bollingerBands({
+        period: parseInt(period),
+        stdDev: parseInt(stdDev),
+        values: closes
+    })
+    return {
+        current: bollingerBands[bollingerBands.length - 1],
+        previous: bollingerBands[bollingerBands.length - 2]
+    }
+}
+
+exports.processIndexes = (indexes, interval, ohlc, callback) => {
+    indexes.map(index => {
+        let result
+        switch (index) {
+            case this.INDEX_KEYS.RSI:
+                result = this.RSI(ohlc.close)
+                break
+
+            case this.INDEX_KEYS.MACD:
+                result = this.MACD(ohlc.close)
+                break
+
+            case this.INDEX_KEYS.SMA:
+                result = this.SMA(ohlc.close)
+                break
+
+            case this.INDEX_KEYS.EMA:
+                result = this.EMA(ohlc.close)
+                break
+
+            case this.INDEX_KEYS.STOCH_RSI:
+                result = this.StochRSI(ohlc.close)
+                break
+
+            case this.INDEX_KEYS.BOLLINGER_BAND:
+                result = this.BollingerBands(ohlc.close)
+                break
+
+        }
+        callback(index, result)
+    })
 }
