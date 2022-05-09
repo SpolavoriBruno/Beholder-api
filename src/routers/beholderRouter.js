34c4c723809baba3dd0794c39c501925ea667d26
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-09 02:06:03 -0300

    Fix route

--- src/routers/beholderRouter.js
+++ src/routers/beholderRouter.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { getBrain, getMemory, getMemoryIndexes } = require('../controllers/beholderController')
 
 router.get('/memory', getMemory)
-router.get('/memoryIndexes', getMemoryIndexes)
+router.get('/memory/indexes', getMemoryIndexes)
 router.get('/brain', getBrain)
 
 module.exports = router
