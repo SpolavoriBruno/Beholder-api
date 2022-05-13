57c8283466497cc5c0f60e7e28ee2f2a255ef018
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-13 18:03:12 -0300

    Routine to Brain Indexes

--- src/controllers/beholderController.js
+++ src/controllers/beholderController.js
@@ -1,8 +1,11 @@
-const { getBrain, getMemories, getMemoryIndexes } = require('../beholder')
+const { getBrain, getMemories, getMemoryIndexes, getBrainIndexes } = require('../beholder')
 
 exports.getMemory = (req, res) => (res.json(getMemories()))
 exports.getBrain = (req, res) => (res.json(getBrain()))
 exports.getMemoryIndexes = (req, res) => {
     const { symbol, index, interval } = req.query
     res.json(getMemoryIndexes(symbol, index, interval))
+}
+exports.getBrainIndexes = (req, res) => {
+    res.json(getBrainIndexes())
 }
--- src/routers/beholderRouter.js
+++ src/routers/beholderRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const { getBrain, getMemory, getMemoryIndexes } = require('../controllers/beholderController')
+const { getBrain, getMemory, getMemoryIndexes, getBrainIndexes } = require('../controllers/beholderController')
 
 router.get('/memory', getMemory)
 router.get('/memory/indexes', getMemoryIndexes)
 router.get('/brain', getBrain)
+router.get('/brain/indexes', getBrainIndexes)
 
 module.exports = router
