45f0ea93c4bb004023f5cf5dece5ff74f086c8d2
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-17 13:01:01 -0300

    Dont send sensitive data to client

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -1,11 +1,20 @@
 const { getSettings, updateSettings } = require('../repositories/settingsRepository')
 const errorHandler = require('../utils/errorHandler')
 
+const sanatizeSettings = settings => {
+    settings = settings.get({ plain: true })
+    delete settings.password
+    delete settings.secretKey
+    delete settings.sendGridKey
+    delete settings.twilioToken
+    return settings
+}
+
 exports.getSettings = (req, res, next) => {
     const id = res.locals.token.id
 
     getSettings(id)
-        .then(settings => res.json(settings))
+        .then(settings => res.json(sanatizeSettings(settings)))
         .catch(e => errorHandler(e, (s, b) => res.status(s).json(b)))
 }
 
@@ -14,7 +23,6 @@ exports.updateSettings = async (req, res, next) => {
     const newSettings = req.body
 
     await updateSettings(id, newSettings)
+        .then(_ => res.sendStatus(202))
         .catch(e => errorHandler(e, (s, b) => res.status(s).json(b)))
-
-    res.sendStatus(200)
 }
