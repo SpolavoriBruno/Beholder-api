3654482b5c4cc29bc8c5861904bb7ebb7f9da8ad
Bruno José Spolavori <bspol.323@gmail.com>
2022-05-30 22:22:28 -0300

    Adapt to get one memory

--- src/controllers/beholderController.js
+++ src/controllers/beholderController.js
@@ -1,8 +1,18 @@
-const { getBrain, getMemories, getMemoryIndexes, getBrainIndexes } = require('../beholder')
+const { getBrain, getMemories, getMemoryIndexes, getBrainIndexes, getMemory } = require('../beholder')
 const { getAnalysisIndexes } = require('../utils/indexes')
 
-exports.getMemory = (req, res) => (res.json(getMemories()))
-exports.getBrain = (req, res) => (res.json(getBrain()))
+exports.getMemory = (req, res) => {
+    const { symbol, index, interval } = req.params
+    if (symbol)
+        res.json(getMemory(
+            symbol.toUpperCase(),
+            index.toUpperCase(),
+            interval
+        ))
+
+    else res.json(getMemories())
+}
+exports.getBrain = (req, res) => res.json(getBrain())
 exports.getMemoryIndexes = (req, res) => {
     const { symbol, index, interval } = req.query
     res.json(getMemoryIndexes(symbol, index, interval))
--- src/routers/beholderRouter.js
+++ src/routers/beholderRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const { getBrain, getMemory, getMemoryIndexes, getBrainIndexes, getAnalysisIndexes } = require('../controllers/beholderController')
 
-router.get('/memory', getMemory)
 router.get('/memory/indexes', getMemoryIndexes)
+router.get('/memory/:symbol?/:index?/:interval?', getMemory)
 router.get('/brain', getBrain)
 router.get('/brain/indexes', getBrainIndexes)
 router.get('/indexes', getAnalysisIndexes)
