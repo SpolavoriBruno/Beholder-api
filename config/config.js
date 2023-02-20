b39c01eacbcb42ce430e7c8fa0426e5ea3946dbf
Bruno J. Spolavori <bspol.323@gmail.com>
2023-02-20 19:12:48 -0300

    rewrite configs

--- config/config.js
+++ config/config.js
@@ -1,15 +1,37 @@
 require('dotenv').config()
 
+const {
+    DB_DIALECT, DB_HOST,
+    DB_NAME, DB_PASSWORD,
+    DB_PORT, DB_USER
+} = process.env
+
 module.exports = {
-    development:{
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host:process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+    development: {
+        username: DB_USER,
+        password: DB_PASSWORD,
+        database: DB_NAME,
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: DB_DIALECT,
         loggin: true
     },
-    test:{},
-    production:{}
+    test: {
+        username: DB_USER,
+        password: DB_PASSWORD,
+        database: `${DB_NAME}-test`,
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: DB_DIALECT,
+        loggin: true
+    },
+    production: {
+        username: DB_USER,
+        password: DB_PASSWORD,
+        database: `${DB_NAME}-prod`,
+        host: DB_HOST,
+        port: DB_PORT,
+        dialect: DB_DIALECT,
+        loggin: true
+    }
 }
